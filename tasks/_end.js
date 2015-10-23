ade5cb925eca3f4b5b56dd8336132e4a70722f45
Acemir Sousa Mendes <acemir@blueshift.cc>
2015-10-23 16:15:35 -0200

    Initial commit in pages branch

--- tasks/_end.js
+++
@@ -1,4 +0,0 @@
-    // Ask almond to synchronously require the module value for 'main' here and
-    // return it as the value to use for the public API for the built file.
-    return require('main');
-}));
